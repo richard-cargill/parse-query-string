b3dbe14e4667424d598ba4d148aede368a46b333
Richard Cargill <richard.r.cargill@gmail.com>
2019-01-07 15:55:10 +0000

    added a public parse method to deal with some basic cases

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-function stripFirstChar(chars, string) {
+function _stripFirstChar(chars, string) {
   const charGroupMatch = new RegExp(`^[${chars}]`);
   return string.trim().replace(charGroupMatch, '');
 }
 
-function stripQueryIndicator(string) {
-  return stripFirstChar('?#&', string);
+function _stripQueryIndicator(string) {
+  return _stripFirstChar('?#&', string);
 }
 
-function helloWorld() {
-  return 'hello world';
+function _keyValueToObj(keyValueString, obj) {
+  const [key, value] = keyValueString.split('=');
+
+  const emptyObj = obj || Object.create(null);
+
+  // If key isn't already set, set it's value
+  if (!(key in emptyObj)) {
+    emptyObj[key] = value;
+  // If key is an array add value to tail
+  } else if (Array.isArray(emptyObj[key])) {
+    emptyObj[key] = [...emptyObj[key], value];
+  // Otherwise create an array
+  } else {
+    emptyObj[key] = [emptyObj[key], value];
+  }
+
+  return emptyObj;
 }
 
-export {stripQueryIndicator, helloWorld};
+function parse(input) {
+  const strippedInput = _stripQueryIndicator(input);
+
+  const parsedObj = strippedInput
+    .split('&')
+    .reduce((accu, curr) => {
+      accu = _keyValueToObj(curr, accu);
+      return accu;
+    }, Object.create(null));
+
+  return parsedObj;
+}
+
+export {
+  _stripQueryIndicator,
+  parse
+};
--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
 import test from 'ava';
-import {stripQueryIndicator} from '..';
+import {_stripQueryIndicator, parse} from '..';
 
 test('remove first char if `?`, `#` or `&`', t => {
-  t.is(stripQueryIndicator('?test=test'), 'test=test');
-  t.is(stripQueryIndicator('#test=test'), 'test=test');
-  t.is(stripQueryIndicator('&test=test'), 'test=test');
-  t.is(stripQueryIndicator('test=test'), 'test=test');
+  t.is(_stripQueryIndicator('?test=test'), 'test=test');
+  t.is(_stripQueryIndicator('#test=test'), 'test=test');
+  t.is(_stripQueryIndicator('&test=test'), 'test=test');
+  t.is(_stripQueryIndicator('test=test'), 'test=test');
+});
+
+test('parse query string with single param', t => {
+  t.deepEqual(parse('?test=test'), {test: 'test'});
+});
+
+test('parse query string with multiple params', t => {
+  t.deepEqual(parse('?test=test&anotherThing=pew pew&other=test'), {other: 'test', test: 'test', anotherThing: 'pew pew'});
+});
+
+test('parse query string with multiple params with the same key', t => {
+  t.deepEqual(parse('?foo=test&foo=pew pew&foo=test'), {foo: ['test', 'pew pew', 'test']});
 });
