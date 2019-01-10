4b34ea27ce64fc4a5eacff394ca58db2c822623e
Richard Cargill <richard.r.cargill@gmail.com>
2019-01-10 12:11:54 +0000

    added in more complex query strings to test and automatic decoding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+const decodeUriComponent = require('decode-uri-component');
+
 function _stripFirstChar(chars, string) {
   const charGroupMatch = new RegExp(`^[${chars}]`);
   return string.trim().replace(charGroupMatch, '');
@@ -9,30 +11,38 @@ function _stripQueryIndicator(string) {
 
 function _keyValueToObj(keyValueString, obj) {
   const [key, value] = keyValueString.split('=');
+  const decodedKey = decodeUriComponent(key);
+  const decodedValue = decodeUriComponent(value);
 
   const emptyObj = obj || Object.create(null);
 
   // If key isn't already set, set it's value
-  if (!(key in emptyObj)) {
-    emptyObj[key] = value;
+  if (!(decodedKey in emptyObj)) {
+    emptyObj[decodedKey] = decodedValue;
   // If key is an array add value to tail
-  } else if (Array.isArray(emptyObj[key])) {
-    emptyObj[key] = [...emptyObj[key], value];
+  } else if (Array.isArray(emptyObj[decodedKey])) {
+    emptyObj[decodedKey] = [...emptyObj[decodedKey], decodedValue];
   // Otherwise create an array
   } else {
-    emptyObj[key] = [emptyObj[key], value];
+    emptyObj[decodedKey] = [emptyObj[decodedKey], decodedValue];
   }
 
   return emptyObj;
 }
 
 function parse(input) {
+  const resultObj = Object.create(null);
+
+  if (!input) {
+    return resultObj;
+  }
+
   const strippedInput = _stripQueryIndicator(input);
 
   return strippedInput
     .split('&')
     .reduce((accu, curr) =>
-      _keyValueToObj(curr, accu), Object.create(null));
+      _keyValueToObj(curr, accu), resultObj);
 }
 
 export {
--- test/test.js
+++ test/test.js
@@ -8,6 +8,10 @@ test('remove first char if `?`, `#` or `&`', t => {
   t.is(_stripQueryIndicator('test=test'), 'test=test');
 });
 
+test('If an empty string is parsed returns an empty `{}`', t => {
+  t.deepEqual(parse(''), {});
+});
+
 test('parse query string with single param', t => {
   t.deepEqual(parse('?test=test'), {test: 'test'});
 });
@@ -18,4 +22,30 @@ test('parse query string with multiple params', t => {
 
 test('parse query string with multiple params with the same key', t => {
   t.deepEqual(parse('?foo=test&foo=pew pew&foo=test'), {foo: ['test', 'pew pew', 'test']});
+});
+
+test('parse a complex query string', t => {
+  t.deepEqual(parse('?q=tree+-swing&l=commderiv&d=taken-20000101-20051231&ct=0&mt=all&adv=1'), {
+    q: 'tree -swing',
+    l: 'commderiv',
+    d: 'taken-20000101-20051231',
+    ct: '0',
+    mt: 'all',
+    adv: '1'
+  });
+});
+
+test('parse nested urls', t => {
+  t.deepEqual(parse('?u=http://www.google.co.uk'), {
+    u: 'http://www.google.co.uk'
+  });
+});
+
+test('parse complex encoded query string', t => {
+  t.deepEqual(parse('?user=Mar%C3%ADa%20Rodr%C3%ADguez&awesome=true&awesomeness=64&ZOMG%2B%26%3D*(=*%5E%25*GMOZ'), {
+    user: 'María Rodríguez',
+    awesome: 'true',
+    awesomeness: '64',
+    'ZOMG+&=*(': '*^%*GMOZ'
+  });
 });
